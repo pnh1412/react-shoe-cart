27262c98c0f1a78fd87e4ada6257c8847e054d92
tonynguyen <tonyjsfreelancer@gmail.com>
2024-03-11 19:16:12 +0700

    reacot code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FaPlus, FaMinus, FaTrash } from 'react-icons/fa';
-
 import './App.css';
 
-function App() {
-  const products = [
-    {
-      id: 0,
-      name: 'Nike Air Zoom Pegasus 36',
-      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png',
-      description: 'The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.',
-      price: '$108.97',
-    },
-    {
-      id: 1,
-      name: 'Nike Air Zoom Pegasus 37',
-      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png',
-      description: 'The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.',
-      price: '$99.99',
-    },
-    {
-      id: 2,
-      name: 'Nike Air Zoom Pegasus 38',
-      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-shield-mens-running-shoe-24FBGb__1_-removebg-preview.png',
-      description: 'The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.',
-      price: '$5000',
-    },
-  ];
+// mock data
+import { dataProducts } from './mocks/data';
 
+function fakeApi(data) {
+  return new Promise(resovle => {
+    setTimeout(() => {
+      resovle(data);
+    }, 1000);
+  })
+}
+function App() {
+  const [products, setProducts] = React.useState([]);
   const [cartItems, setCartItems] = useState([]);
+
+  // initial data products
+  React.useEffect(() => {
+    fakeApi(dataProducts)
+      .then(data => setProducts(data.data))
+  }, [])
 
   const addToCart = (product) => {
     setCartItems([...cartItems, { ...product, quantity: 1 }]);
---
+++ src/mocks/data.js
@@ -0,0 +1,25 @@
+export const dataProducts = {
+    "data": [
+        {
+            "id": 0,
+            "name": "Nike Air Zoom Pegasus 36",
+            "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
+            "description": "The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.",
+            "price": "$108.97"
+          },
+          {
+            "id": 1,
+            "name": "Nike Air Zoom Pegasus 37",
+            "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
+            "description": "The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.",
+            "price": "$99.99"
+          },
+          {
+            "id": 2,
+            "name": "Nike Air Zoom Pegasus 38",
+            "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-shield-mens-running-shoe-24FBGb__1_-removebg-preview.png",
+            "description": "The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.",
+            "price": "$5000"
+          }
+    ]
+  }
